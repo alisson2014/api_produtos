9b0bdbc62c0970dbaae9909fba293cea69d96a89
Alisson Vinícius <almeidaalisson2014@gmail.com>
2023-05-10 17:55:02 -0300

    adicionei http_response_code

--- list/categories.php
+++ list/categories.php
@@ -2,21 +2,23 @@
 
 use Produtos\Action\Service\Lister;
 
+require_once "../config.php";
+require_once "../autoload.php";
+
 //Cabeçalhos para requisição HTTP
 header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Origin: http://localhost:3000");
 header("Access-Control-Allow-Methods: GET");
 header("Content-Type: application/json; charset=UTF-8");
 
-require_once "../config.php";
-require_once "../autoload.php";
-
 $query = new Lister("SELECT * FROM subcategoria ORDER BY id ASC", $pdo);
 $consult = $query->sqlConsult();
+$http_response_code = 502;
 
 if ($query->hasData()) {
+    $http_response_code = 200;
     $data = $query->returnsData();
     echo json_encode($data);
 }
 
-http_response_code(200);
+http_response_code($http_response_code);
