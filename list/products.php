0a6bf6afe661e0816b121e50afae8c9b85e97620
Alisson Vinícius <almeidaalisson2014@gmail.com>
2023-05-20 17:57:00 -0300

    criei o get dos produtos

---
+++ list/products.php
@@ -0,0 +1,25 @@
+<?php
+
+use Produtos\Action\Service\Lister;
+
+require_once "../config.php";
+require_once "../autoload.php";
+
+//Cabeçalhos para requisição HTTP
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Methods: GET");
+header("Content-Type: application/json; charset=UTF-8");
+
+$sqlQuery = "SELECT p.*,s.nome as nomeCategoria FROM produto as p JOIN subcategoria as s ON s.id = p.subcategoria";
+$list = new Lister($sqlQuery, $pdo);
+$consult = $list->sqlConsult();
+$http_response_code = 502;
+
+if ($list->hasData()) {
+    $http_response_code = 200;
+    $data = $list->returnsData();
+    echo json_encode($data);
+}
+
+http_response_code($http_response_code);
