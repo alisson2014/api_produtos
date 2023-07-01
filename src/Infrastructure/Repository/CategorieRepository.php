0b567d1580e6ef7b346d8b641aec22dda5856191
Alisson Vinícius <almeidaalisson2014@gmail.com>
2023-07-01 15:54:36 -0300

    Apliquei transações no repositorio

--- src/Infrastructure/Repository/CategorieRepository.php
+++ src/Infrastructure/Repository/CategorieRepository.php
@@ -7,14 +7,18 @@ namespace Produtos\Action\Infrastructure\Repository;
 use PDO;
 use Produtos\Action\Domain\Model\Categorie;
 use Produtos\Action\Domain\Repository\CategorieRepo;
+use Produtos\Action\Service\TryAction;
 
 class CategorieRepository implements CategorieRepo
 {
+    use TryAction;
+
     public function __construct(
         private PDO $pdo
     ) {
     }
 
+    /** @return Categorie[] */
     public function all(): array
     {
         $categorieList = $this->pdo
@@ -29,37 +33,39 @@ class CategorieRepository implements CategorieRepo
 
     public function add(Categorie $categorie): bool
     {
+        $this->pdo->beginTransaction();
         $sql = "INSERT INTO subcategoria (id, nome) VALUES (NULL, ?)";
         $stmt = $this->pdo->prepare($sql);
         $stmt->bindValue(1, $categorie->categorieName);
-        $status = $stmt->execute();
+        $status = $this->tryAction($stmt, true);
+        $result = $status["result"];
 
-        if ($status) {
-            $lastId = $this->pdo->lastInsertId();
-            $categorie->setId(intval($lastId));
+        if ($result) {
+            $categorie->setId(intval($status["id"]));
         }
 
-        return $status;
+        return $result;
     }
 
     public function remove(int $id): bool
     {
+        $this->pdo->beginTransaction();
         $sql = "DELETE FROM subcategoria WHERE id = ?";
         $stmt = $this->pdo->prepare($sql);
         $stmt->bindValue(1, $id, PDO::PARAM_INT);
-        $result = $stmt->execute();
+        $result = $this->tryAction($stmt);
 
         return $result;
     }
 
     public function update(Categorie $categorie): bool
     {
+        $this->pdo->beginTransaction();
         $sql = "UPDATE subcategoria SET nome = :nome WHERE id = :id LIMIT 1;";
         $stmt = $this->pdo->prepare($sql);
-
         $stmt->bindValue(":nome", $categorie->categorieName);
         $stmt->bindValue(":id", $categorie->id, PDO::PARAM_INT);
-        $status = $stmt->execute();
+        $status = $this->tryAction($stmt);
 
         return $status;
     }
---
+++ src/Service/TryAction.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Produtos\Action\Service;
+
+trait TryAction
+{
+    /**
+     * @param \PDOStatement|bool $stmt
+     * @param bool $returnId
+     * @return array|bool
+     */
+    private function tryAction(
+        \PDOStatement|bool $stmt,
+        bool $returnId = false,
+    ): array|bool {
+        try {
+            $result = $stmt->execute();
+
+            if ($returnId) {
+                $lastId = $this->pdo->lastInsertId();
+            }
+
+            $this->pdo->commit();
+        } catch (\PDOException $e) {
+            echo $e->getMessage();
+            $this->pdo->rollBack();
+        }
+
+        if ($returnId) {
+            return [
+                "result" => $result,
+                "id" => $lastId
+            ];
+        }
+
+        return $result;
+    }
+}
