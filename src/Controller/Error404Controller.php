339200974bad4b3fb9cf6e57a164cb8134967488
Alisson Vinícius <almeidaalisson2014@gmail.com>
2023-10-27 22:08:41 -0300

    ♻️Ajuste no modulo de erro 404

--- src/Controller/Error404Controller.php
+++ src/Controller/Error404Controller.php
@@ -9,7 +9,7 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
-class Error404Controller implements RequestHandlerInterface
+final class Error404Controller implements RequestHandlerInterface
 {
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
