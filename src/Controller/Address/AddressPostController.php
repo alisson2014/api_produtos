e8d87be29b428d4826ab4199d9f5ea227b5a09c2
Alisson Vinícius <almeidaalisson2014@gmail.com>
2023-10-30 20:41:59 -0300

    ✨Terminei a implementação da rota de cadastro de endereços

--- src/Controller/Address/AddressPostController.php
+++ src/Controller/Address/AddressPostController.php
@@ -12,6 +12,13 @@ use Psr\Http\Server\RequestHandlerInterface;
 
 final class AddressPostController implements RequestHandlerInterface
 {
+    private ?int $cep;
+    private ?string $numero;
+    private ?string $localidade;
+    private ?string $uf;
+    private ?string $bairro;
+    private ?string $logradouro;
+    
     public function __construct(
         private AddressRepository $addressRepository
     ) {
@@ -20,39 +27,66 @@ final class AddressPostController implements RequestHandlerInterface
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
         $body = json_decode($request->getBody()->getContents());
+        $this->cep = isset($body->cep) ? filter_var($body->cep, FILTER_VALIDATE_INT) : null;
+        $this->numero = isset($body->numero) ? $body->numero : null;
+
+        if (empty($this->cep) || strlen(strval($this->cep)) < 8) {
+            return Helper::invalidRequest("CEP inválido!");
+        } else if (empty($this->numero)) {
+            return Helper::invalidRequest("Insira o número da residência!");
+        }
 
         if ($body->isByCep) {
-            $cep = isset($body->cep) ? filter_var($body->cep, FILTER_VALIDATE_INT) : null;
-            $numero = isset($body->numero) ? $body->numero : null;
-
-            if (empty($cep) || strlen((string)$cep) < 8) {
-                return Helper::invalidRequest("CEP inválido!");
-            } else if (empty($numero)) {
-                return Helper::invalidRequest("Insira o número da residência!");
-            }
-
-            $address = $this->addressRepository->findByCep($cep);
-
-            if (empty($address)) {
-                return Helper::invalidRequest("CEP não encontrado na base de dados dos correios!");
-            }
-
-            $address = new Address(
-                $cep,
-                $address["uf"],
-                $address["localidade"],
-                $address["bairro"],
-                $address["logradouro"],
-                $numero
-            );
-
-            $this->addressRepository->add($address);
+            return $this->addByCep();
         }
 
-        if (empty($cep) || strlen((string)$cep) < 8) {
-            return Helper::invalidRequest("CEP inválido!");
+        $this->localidade = isset($body->cidade) ? $body->cidade : null;
+        $this->uf = isset($body->uf) ? $body->uf : null;
+        $this->bairro = isset($body->bairro) ? $body->bairro : null;
+        $this->logradouro = isset($body->logradouro) ? $body->logradouro : null;
+
+        if (empty($this->localidade)) {
+            return Helper::invalidRequest("Preencha a cidade.");
+        } else if (empty($this->uf)) {
+            return Helper::invalidRequest("Preencha o estado.");
+        } else if (empty($this->bairro)) {
+            return Helper::invalidRequest("Preencha o bairro.");
+        } else if (empty($this->logradouro)) {
+            return Helper::invalidRequest("Preencha o logradouro.");
         }
 
-        return Helper::showResponse($this->addressRepository->findByCep($cep));
+        return $this->addAddress();
+    }
+
+    private function addByCep(): ResponseInterface {
+        $address = $this->addressRepository->findByCep($this->cep);
+
+        if (empty($address)) {
+            return Helper::invalidRequest("CEP não encontrado na base de dados dos correios!");
+        }
+
+        $this->uf = $address["uf"];
+        $this->localidade = $address["localidade"];
+        $this->bairro = $address["bairro"];
+        $this->logradouro = $address["logradouro"];
+
+        return $this->addAddress();
+    }
+
+    private function addAddress(): ResponseInterface {
+        $address = new Address(
+            $this->cep,
+            $this->uf,
+            $this->localidade,
+            $this->bairro,
+            $this->logradouro,
+            $this->numero
+        );
+
+        if (!$this->addressRepository->add($address)) {
+            return Helper::internalError();
+        }
+
+        return Helper::showStatus("Endereço cadastrado com sucesso", 201);
     }
 }
